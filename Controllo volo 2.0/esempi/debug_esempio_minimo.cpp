94931ecdd3d90317cd862075163e77db7fec5181
noearchimede <noearchimede@gmail.com>
2017-07-10 15:25:26 +0200

    Aggiunta la classe Debug
    Questa classe è pensata per essere alla base del debug dell'intero
    programma. Insieme ad essa ho aggiunto un programma di esempio.

---
+++ Controllo volo 2.0/esempi/debug_esempio_minimo.cpp
@@ -0,0 +1,88 @@
+/**
+\file
+
+\date 5 - 9 luglio 2017
+
+\brief Esempio di utilizzo della classe Debug.
+
+Questo programma contiene il minimo necessario per poter usare correttamente la
+classe. Oltre a quelle presenti qui, la classe diverse altre funzioni secondarie
+(ad esempio quelle per modifcarne le  impostazioni).
+
+Questo programma, compilato con la libreria Debug allo stato del 9 luglio 2017
+e caricato su un Arduino UNO, ha funzionato.
+
+
+Hardware necessario
+-------------------
+*  pin 13 - LED
+*  interfaccia seriale - computer
+
+*/
+
+#include "Arduino.h"
+
+
+#include <Debug.h>
+
+//------------------------------------------------------------------------------
+//Liste di #efine per i messaggio, gli errori e gli errori fatali.
+
+//messaggi
+#define MESS_ESEMPIO_1  1
+#define MESS_ESEMPIO_2  2
+
+//errori
+#define ERR_ESEMPIO     1
+
+//errori fatali
+#define ERRFAT_ESEMPIO  1
+//------------------------------------------------------------------------------
+
+
+// Crea un'istanza della classe, collegandovi un'interfaccia seriale (l'unica
+// disponibile, nel caso di ATmega328P) e un LED (in questo caso collegato al pin 13).
+// Si presume che Serial e del LED siano a completa disposizione della classe, e
+// cioè che non siano mai usati altrove nel programma.
+// La classe stessa non impedisce che essi siano comunque usati, ma il comportamento
+// in caso di "interferenze" non è definito.
+Debug debug(&Serial, 13);
+
+
+
+void setup () {
+
+    // La classe usa Serial, ma non lo inizializza al suo interno. Quindi è necessario
+    // chimare una volta all'inizio del programma questa funzione. ovviamente
+    // prima di essa non potranno essere inviate notifiche (questo comunque non
+    // causerebbe nessun errore, semplicemente non si vedrebbero sul monitor seriale).
+    Serial.begin(115200);
+
+}
+
+
+
+void loop () {
+
+    // Questa funzione controlla il LED.
+    // ATTENZIONE! Deve essere chiamata regolarmente e il più frequentemente possibile!
+    debug.controllaLed();
+
+
+
+    //--------------------------------------------------------------------------
+    // Funzioni di notifica, da usare in qualsiasi punto del programma
+
+    //un messaggio (con "codice") (1)
+    debug.messaggio(MESS_ESEMPIO_2, 4);
+
+    //un errore (senza "codice" (codice 0 = senza codice))
+    debug.errore(ERR_ESEMPIO, 0);
+
+    //un errore fatale (senza "codice")
+    //il programma resterà per sempre bloccato nella funzione seguente
+    debug.erroreFatale(ERRFAT_ESEMPIO);
+
+    //--------------------------------------------------------------------------
+
+}
---
+++ Controllo volo 2.0/lib/Debug/Debug.h
@@ -0,0 +1,208 @@
+/**
+\file
+
+\brief Header della classe `Debug`
+\date 5 - 9 luglio 2017
+*/
+
+/**
+\class Debug
+\brief Classe utile per il debug di un prgramma qualsiasi.
+
+Informazioni generali
+---------------------
+
+Questa classe permette di stampare sul monitor seriale tre tipi di notifica:
+- messaggi
+- errori
+- errori fatali (vedi sotto)
+
+Le notifiche non sono rappresentate da stringhe di testo (che occuperebbero
+troppa memoria su ATmega328), ma da numeri. Occorre quindi creare un file di
+`#define` per associare ad ogni errore o messaggio un numero.
+Inoltre per ogni notifica inviata sul monitor seriale si accende un LED, che
+rimane accesso per un tempo differente a seconda del tipo di notifica.
+
+I messaggi e gli errori sono molto simili tra loro, differiscono solo per la
+visibilità sia sul monitor seriale sia tramite il LED (che si accende più a
+lungo per segnalare glie errori).
+Gli errori fatali invece sono gestiti in modo molto diverso: quando il programma
+chiama la funzione `erroreFatale` di questa classe si blocca per sempre nel loop
+contenuto in essa.
+
+
+Linee guida per lo sviluppo
+---------------------------
+
+La classe `Debug` deve avere il monopolio della comunicazione con il computer.
+I suoi messaggi possono comprendere:
+- errori
+- checkpoints
+- calcoli svolti: risultati finali o parziali
+- qualsiasi tipo di input esterno
+    - messaggi ricevuti da sensori, telecomando, ...
+
+Oltre a servire per il riconoscimento degli errori "attuali", la classe deve
+essere scritta e usata in modo da generare un _log_ di tutto ciò che accade nel
+programma. Questo _log_ invece di essere inviato al computer (che per la maggior
+parte del tempo si presume non sia connesso al sistema) potrebbe anche essere
+salvato da qualche parte sul robot per una lettura successiva.
+
+Un compito secondario ma comunque importante di questa classe è bloccare il
+sistema in caso di errore fatale.
+
+
+Istruzioni per l'utilizzo
+-------------------------
+\example debug_esempio_minimo.cpp
+Esempio dell'utilizzo più semplice possibile della classe Debug.
+\include lib/Debug/examples/debug_esempio_minimo.cpp
+*/
+
+
+#ifndef Debug_h
+#define Debug_h
+
+#include "Arduino.h"
+
+/// \name Impostazioni di default della classe `Debug`
+/// @{
+
+///Attiva la comunicazione seriale (`true`)?
+#define DEBUG_DEFAULT_USA_SERIAL                true
+///Stampa anche i messaggi (`true`) o solo gli errori?
+#define DEBUG_DEFAULT_STAMPA_MESS               true
+///Stampa solo il minimo indispensabille (`true`) o produci un output più leggibile?
+#define DEBUG_DEFAULT_STAMPA_MINIMO             false
+///\brief Ignora il codice eventualmente associato alle notifiche
+///\warning ha effetto solo se anche `DEBUG_DEFAULT_STAMPA_MINIMO` è `true`
+#define DEBUG_DEFAULT_IGNORA_CODICE             false
+///Dopo ogni notifica aspetta che il led sia spento prima di proseguire
+#define DEBUG_DEFAULT_ASPETTA_FINE_NOTIFICA     false
+
+///Tempo per cui il LED resta acceso per ogni messaggio (in millisecondi)
+#define DEBUG_DEFAULT_LUCE_MESS                 50
+///Tempo per cui il LED resta acceso per ogni errore (in millisecondi)
+#define DEBUG_DEFAULT_LUCE_ERR                  300
+///Durata della luce (e del "buio") nel lampeggiamento in caso di errore fatale
+#define DEBUG_DEFAULT_LUCE_ERRFAT               1500
+///Se `DEBUG_ASPETTA_FINE_NOTIFICA`è `true`, questo è il tempo minimo tra due segnali LED
+#define DEBUG_DEFAULT_DURATA_BUIO_DOPO_NOTIFICA 30
+
+///Tempo di attesa prima che il monitor seriale sia resettato in caso di errore fatale
+#define DEBUG_DEFAULT_TEMPO_RESET_SERIAL        5000
+///\brief Velocità della comunicazione seriale con il computer a partire da
+///`DEBUG_DEFAULT_TEMPO_RESET_SERIAL` ms dopo un errore fatale, in Baud
+#define DEBUG_DEFAULT_BAUD_SERIAL               115200
+
+/// @}
+
+
+class Debug {
+
+  public:
+
+    ///Assegna tutti i valori di default e inizializza la comunicazione seriale
+    Debug(HardwareSerial*, int);
+
+    /// \name Funzioni principali
+    /// @{
+
+    ///Stampa un messaggio
+    void messaggio(int, long = 0, bool = false);
+    ///Stampa un errore
+    void errore(int, long = 0, bool = false);
+    ///Blocca il brogramma e segnala un errore fatale
+    void erroreFatale(int, long = 0);
+
+    ///Controlla il led; se è acceso ed è ora di spegnerlo lo spegne
+    void controllaLed();
+
+    /// @}
+
+
+    /// \name Funzioni di modifica impostazioni
+    /// @{
+
+    ///Cambia l'impostazione `usaSerial`
+    void impostaUsaSerial(bool);
+    ///Cambia l'impostazione `stampaMessaggi`
+    void impostaStampaMessaggi(bool);
+    ///Cambia l'impostazione `stampaMinimo`
+    void impostaStampaMinimo(bool);
+    ///Cambia i tempi di accensione del led per i vari tipi di notifica
+    void impostaDurateLuci(int, int, int); //parametri nell'ordine: mess-err-errFat
+    ///Cambia il tempo minimo di attesa tra due segnali luminosi se DEBUG_ASPETTA_FINE_NOTIFICA
+    void impostaDurataBuioDopoNotifica(int);
+    ///Chiedi alla classe se sta usando la comunicazione seriale
+    bool staUsandoSerial();
+
+    /// @}
+
+  private:
+
+    ///accende il led e imposta correttametne le variabili associate ad esso
+    void accendiLed(int); //int: durata luce. Vedi anche commento sotto.
+    ///spegne il led e imposta correttametne le variabili associate ad esso
+    void spegniLed(); //il pin è sottinteso: questa classe ne usa solo uno, "_pinLed"
+
+    ///Blocca il programma fino a quando il LED si spegne più qualche ms.
+    void aspettaFineNotifica();
+
+
+    //###### VARIABILI ######
+
+    /// \name Controllo del LED
+    /// @{
+
+    ///Pin del led
+    int _pinLed;
+    ///Durata della luce per i messaggi
+    int _durataLuceMessaggio;
+    ///Durata della luce per gli errori
+    int _durataLuceErrore;
+    ///Durata del lempeggiamento per gli errori fatali
+    int _durataLuceErroreFatale;
+    ///Attesa (opzionale) dopo lo spegnimento del LED
+    int _durataBuioDopoNotifica;
+
+    ///Stato attuale del led
+    bool _ledAcceso;
+    ///Ora di accensione del led. Non ha senso se `_ledAcceso == false`
+    unsigned long _tempoAccensioneLed;
+    ///Tempo per cui deve stare attualmente acceso il led. Non ha senso se `_ledAcceso == false`
+    int _durataLuceLed;
+
+    /// @}
+
+
+    /// \name Controllo di Serial
+    /// @{
+
+    ///Riferimento a `Serial` per questa classe.
+    HardwareSerial* _serial;
+    ///La classe può usare la porta seriale?
+    bool _usaSerial;
+    ///Velocità della comunicazione seriale in caso di errore fatale (in Baud)
+    long _baudComunicazioneSeriale;
+
+    /// @}
+
+
+    /// \name Impostazioni generali
+    /// @{
+
+    ///Dopo ogni notifica blocca il programma fino a quando il LED si spegne
+    bool _aspettaFineNotifica;
+    ///Stampa anche i messaggi (true) solo gli errori (false)?
+    bool _stampaMessaggi;
+    ///Stampa solo il minimo indispensabille (true)
+    bool _stampaMinimo;
+    ///Ignora il codice associato ai messaggi. Ha effetto solo se `_stampaMinimo == true`
+    bool _ignoraCodice;
+
+    /// @}
+
+};
+
+#endif
