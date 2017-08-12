1644d5c250a09de7c1eba924689f4346da436bba
noearchimede <noearchimede@gmail.com>
2017-08-12 17:59:46 +0200

    Aggiunti commenti alla classe Comunicazione

--- Controllo volo 2.0/lib/Debug/Comunicazione.cpp
+++ Controllo volo 2.0/lib/Debug/Comunicazione.cpp
@@ -1,16 +1,20 @@
 
-
 #include "Comunicazione.hpp"
 
 Comunicazione::Comunicazione(HardwareSerial& hwserial) :
 _hwserial(hwserial),
-_attivo(false)
+_baud(9600),
+_comunicazioneAbilitata(true)
 {}
 
 
-void Comunicazione::abilita(bool attivo) {
-    _attivo = attivo;
+void Comunicazione::abilita() {
+    _comunicazioneAbilitata = true;
 }
+void Comunicazione::disabilita() {
+    _comunicazioneAbilitata = false;
+}
+
 void Comunicazione::impostaVelocita(long baud) {
     _baud = baud;
 }
@@ -20,32 +24,36 @@ long Comunicazione::ottieniVelocita() {
 
 
 void Comunicazione::begin(long baud, byte config){
-    if(_attivo) {
+    _baud = baud;
+    if(_comunicazioneAbilitata) {
         if(config == 0xFF)
         _hwserial.begin(baud);
         else
         _hwserial.begin(config);
     }
-    _baud = baud;
+
 }
+void Comunicazione::begin() {
+    begin(_baud);
+}
+
 void Comunicazione::end(){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.end();
-    _attivo = false;
 }
 
 
 
 
 int Comunicazione::available(){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     return _hwserial.available();
     else return 0;
 }
 
 
 int Comunicazione::read(){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     return _hwserial.read();
     else return 0;
 }
@@ -54,38 +62,38 @@ int Comunicazione::read(){
 
 
 void Comunicazione::print(const char val[]){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(const String& val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(uint8_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(int8_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(uint16_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(int16_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(uint32_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(int32_t val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
 void Comunicazione::print(float val){
-    if(_attivo)
+    if(_comunicazioneAbilitata)
     _hwserial.print(val);
 }
--- Controllo volo 2.0/lib/Debug/Comunicazione.hpp
+++ Controllo volo 2.0/lib/Debug/Comunicazione.hpp
@@ -1,3 +1,16 @@
+/** \file
+
+\brief Header della classe `Comunicazione`, una classe di supporto per Debug
+\date 12 agosto 2017
+
+La classe `Comunicazione` si occupa di tutta la comunicazione tra la classe
+Debug e l'utente, esclusi i segnali luminosi del LED.
+È sostanzialmente un alias di HardwareSerial con qualche funzione aggiuntiva. Il
+vantaggio di usare questa invece di HardwareSerial è che così è molto più facile
+sostituire l'interfaccia seriale con un'altra o aggiungerne una nuova.
+
+*/
+
 #ifndef comunicazione_hpp
 #define comunicazione_hpp
 
@@ -9,15 +22,31 @@ public:
 
     Comunicazione(HardwareSerial& hwserial);
 
-    void abilita(bool);
+    ///\brief abilita la comunicazione
+    ///\details Se la comunicazione è disabilitata le funzioni di comunicazioni
+    ///termineranno appena chiamate.
+    void abilita();
+    ///cfr. `abilita()`
+    void disabilita();
+
+    ///imposta la velocità della comunicazione seriale
     void impostaVelocita(long);
+    ///restituisce la velocità della comunicazione seriale attuale
     long ottieniVelocita();
 
-    //0xFF è un valore non valido per `config`. Lascio la scelta del default a Serial.
+    ///\brief inizializza la comunicazione seriale.
+    ///\param baud velocità della comunicazione
+    ///\param config configurazione di `HardwareSerial`
+    ///\note 0xFF è un valore non valido per `config`. Lascio la scelta del default a Serial.
     void begin(long, byte = 0xFF);
+    ///come `begin(long, byte = 0xFF);`; usa il valore di `_baud` come velocità
+    void begin();
+    ///termina la comunicazione seriale
     void end();
 
+    ///nuovi dati disponibili?
     int available();
+    ///preleva un carattere dal buffer di caratteri in arrivo
     int read();
 
     void print(const char[]);
@@ -30,12 +59,17 @@ public:
     void print(int32_t);
     void print(float);
 
+
 private:
 
+    ///collegamento a un'istanza di HardwareSerial (tipicamente `Serial`)
     HardwareSerial& _hwserial;
 
+    ///velocità attuale della comunicazione seriale
     long _baud;
-    bool _attivo;
+    ///Se la comunicazione è disabilitata le funzioni di comunicazione escono
+    ///subito grazie a un singolo if
+    bool _comunicazioneAbilitata;
 
 };
 
