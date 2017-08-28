bfd0aea5bd3db5b36e13f6b75f74bfe6f31aac1a
noearchimede <noearchimede@gmail.com>
2017-08-28 11:32:59 +0200

    Comunicazione: aggiunta la possibilità di stampare in basi diverse da 10

---
+++ Controllo volo 2.0/esempi/Prova_Assegna/Comunicazione.cpp
@@ -0,0 +1,114 @@
+
+
+#include "Comunicazione.hpp"
+
+Comunicazione::Comunicazione(HardwareSerial& hwserial) :
+_abilita(true),
+_hwserial(hwserial),
+_baud(9600)
+{
+}
+
+void Comunicazione::begin(long baud, byte config) {
+    _baud = baud;
+    if(_abilita) {
+        if(config == 0xFF)
+        _hwserial.begin(baud);
+        else
+        _hwserial.begin(baud, config);
+    }
+
+}
+void Comunicazione::begin() {
+    begin(_baud);
+}
+
+void Comunicazione::end() {
+    if(_abilita)
+    _hwserial.end();
+}
+
+
+
+void Comunicazione::abilita() {
+    _abilita = true;
+}
+void Comunicazione::disabilita() {
+    _abilita = false;
+}
+bool Comunicazione::abilitato() {
+    return _abilita;
+}
+
+void Comunicazione::impostaVelocita(long baud) {
+    _baud = baud;
+}
+long Comunicazione::ottieniVelocita() {
+    return _baud;
+}
+
+
+
+int Comunicazione::available() {
+    if(_abilita)
+    return _hwserial.available();
+    else return 0;
+}
+
+
+int Comunicazione::read() {
+    if(_abilita)
+    return _hwserial.read();
+    else return 0;
+}
+
+
+void Comunicazione::print(bool val) {
+    if(!_abilita) return;
+    if(val == true) _hwserial.print("true");
+    if(val == false) _hwserial.print("false");
+}
+
+void Comunicazione::print(uint8_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+void Comunicazione::print(uint16_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+void Comunicazione::print(uint32_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+void Comunicazione::print(int8_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+void Comunicazione::print(int16_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+void Comunicazione::print(int32_t val, int base) {
+    if(_abilita)
+    _hwserial.print(val, base);
+}
+
+void Comunicazione::print(double val, int decimali) {
+    if(_abilita)
+    _hwserial.print(val, decimali);
+}
+
+void Comunicazione::print(char val) {
+    if(_abilita)
+    _hwserial.print(val);
+}
+void Comunicazione::print(const char val[]) {
+    if(_abilita)
+    _hwserial.print(val);
+}
+void Comunicazione::print(const String& val) {
+    if(_abilita)
+    _hwserial.print(val);
+}
+
---
+++ Controllo volo 2.0/esempi/Prova_Assegna/Comunicazione.hpp
@@ -0,0 +1,92 @@
+/** \file
+
+\brief Header della classe `Comunicazione`, una classe di supporto per Debug
+\date 12 agosto 2017
+
+La classe `Comunicazione` si occupa di tutta la comunicazione tra la classe
+Debug e l'utente, esclusi i segnali luminosi del LED.
+È sostanzialmente un alias di HardwareSerial con qualche funzione aggiuntiva. Il
+vantaggio di usare questa invece di HardwareSerial è che così è molto più facile
+sostituire l'interfaccia seriale con un'altra o aggiungerne una nuova.
+
+*/
+#ifndef Debug_Comunicazione_hpp
+#define Debug_Comunicazione_hpp
+
+#include "Arduino.h"
+
+class Comunicazione {
+
+public:
+
+    Comunicazione(HardwareSerial&);
+
+    ///inizializza la comunicazione seriale.
+    /**
+    \param baud velocità della comunicazione
+    \param config configurazione di `HardwareSerial`
+    \note 0xFF è un valore non valido per `config`. Lascio la scelta del default a Serial.
+    */
+    void begin(long, byte = 0xFF);
+    ///come `begin(long, byte = 0xFF);`; usa il valore di `_baud` come velocità
+    void begin();
+    ///termina la comunicazione seriale
+    void end();
+
+    ///abilita la comunicazione
+    /**
+    Se la comunicazione è disabilitata le funzioni di comunicazioni
+    termineranno appena chiamate.
+    */
+    void abilita();
+    ///cfr. `abilita()`
+    void disabilita();
+    ///è abilitato o no?
+    bool abilitato();
+
+
+    ///imposta la velocità della comunicazione seriale
+    void impostaVelocita(long);
+    ///restituisce la velocità della comunicazione seriale attuale
+    long ottieniVelocita();
+
+    ///nuovi dati disponibili?
+    int available();
+    ///preleva un carattere dal buffer di caratteri in arrivo
+    int read();
+
+    ///stampa l'argomento passato alla funzione
+    void print(bool);
+    void print(uint8_t, int = 10);
+    void print(uint16_t, int = 10);
+    void print(uint32_t, int = 10);
+    void print(int8_t, int = 10);
+    void print(int16_t, int = 10);
+    void print(int32_t, int = 10);
+    void print(double, int = 2);
+
+    void print(char);
+    void print(const char[]);
+    void print(const String&);
+
+
+private:
+
+    ///Stato del sistema di comunicazione
+    /*
+    Se la comunicazione è disabilitata le funzioni di comunicazione escono
+    subito grazie a un singolo if
+    */
+    bool _abilita;
+
+    ///collegamento a un'istanza di HardwareSerial (tipicamente `Serial`)
+    HardwareSerial& _hwserial;
+
+    ///velocità attuale della comunicazione seriale
+    long _baud;
+
+};
+
+
+#endif
+
